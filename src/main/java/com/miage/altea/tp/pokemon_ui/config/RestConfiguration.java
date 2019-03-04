a85bb8341045f705242effc63c8bf7165c988884
perrier <thomas.perrier.etu@univ-lille.fr>
2019-03-04 09:37:21 +0100

    pokemon-ui

--- src/main/java/com/miage/altea/tp/pokemon_ui/config/RestConfiguration.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/config/RestConfiguration.java
@@ -2,14 +2,29 @@ package com.miage.altea.tp.pokemon_ui.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class RestConfiguration {
 
     @Bean
-    RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
+    RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
 
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add((request, body, execution) -> {
+            request.getHeaders().add("Accept-Language", LocaleContextHolder.getLocale().getLanguage());
+            return execution.execute(request, body);
+        });
+
+        restTemplate.setInterceptors(interceptors);
+
+        return restTemplate;
+    }
 }
+
--- src/main/java/com/miage/altea/tp/pokemon_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
@@ -3,26 +3,46 @@ package com.miage.altea.tp.pokemon_ui.pokemonTypes.service;
 import com.miage.altea.tp.pokemon_ui.pokemonTypes.bo.PokemonType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class PokemonTypeServiceImpl implements PokemonTypeService {
 
-    RestTemplate restTemplate;
-    String actualUrl;
+    private RestTemplate restTemplate;
+    private String url;
 
+    @Override
     public List<PokemonType> listPokemonsTypes() {
-        PokemonType[] pokemons = restTemplate.getForObject(actualUrl+"/pokemon-types/", PokemonType[].class);
-        return Arrays.asList(pokemons);
+        PokemonType[] objects = this.restTemplate.getForObject(this.url+"/pokemon-types/", PokemonType[].class);
+
+        if (objects != null) {
+            return Arrays.asList(objects);
+        } else {
+            throw new NullPointerException();
+        }
     }
 
-    public PokemonType getPokemon(int id) {
-        PokemonType pok = restTemplate.getForObject(actualUrl+"/pokemon-types/?id=", PokemonType.class, id);
-        return pok;
+    @Override
+    public PokemonType getPokemonByTypeId(Integer id) {
+        PokemonType pokemonType = this.restTemplate.getForObject(this.url+"/pokemon-types", PokemonType.class, id);
+
+        if (pokemonType != null) {
+            return pokemonType;
+        } else {
+            throw new NullPointerException();
+        }
     }
 
     @Autowired
@@ -31,7 +51,7 @@ public class PokemonTypeServiceImpl implements PokemonTypeService {
     }
 
     @Value("${pokemonType.service.url}")
-    void setPokemonTypeServiceUrl(String pokemonServiceUrl) {
-        this.actualUrl = pokemonServiceUrl;
+    public void setPokemonTypeServiceUrl(String pokemonServiceUrl) {
+        this.url = pokemonServiceUrl;
     }
 }
